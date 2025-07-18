e826321b010474e3e0374124c2527b1fe8116df0
japuus <50738467+japuus@users.noreply.github.com>
2025-07-18 09:38:56 +0300

    Add function for equal sign button

--- script.js
+++ script.js
@@ -1,39 +1,58 @@
-let storedValue = "";
-let confirmedValue1 = "";
-let confirmedValue2
+let firstValue = "";
+let firstValueConfirmed = "";
+const display = document.querySelector(".display");
+const display2 = document.querySelector(".display2");
 
-const button1 = document.querySelector(".one");
-button1.addEventListener("click", () => {
-    storedValue += 1;
-    console.log("storedValue is "+ storedValue);
-    })
+let result;
 
-const button2 = document.querySelector(".two");
-button2.addEventListener("click", () => {
-    storedValue += 2;
-    console.log("storedValue is "+ storedValue);
+const buttonOne = document.querySelector(".one");
+buttonOne.addEventListener("click", () => {
+    firstValue += 1;
+    console.log("firstValue is " + firstValue);
+    display.textContent = firstValue;
 });
 
-const button3 = document.querySelector(".three");
-button3.addEventListener("click", () => {
-    storedValue += 3;
-    console.log("storedValue is "+ storedValue);
-});
+const buttonTwo = document.querySelector(".two");
+buttonTwo.addEventListener("click", () => {
+    firstValue += 2;
+    console.log("firstValue is " + firstValue);
+    display.textContent = firstValue;
+})
 
-const buttonPlus = document.querySelector(".plus");
-buttonPlus.addEventListener("click", () => {
-    // if confirmed value is not "" then storedValue becomes confirmedValue2
-    if ((confirmedValue1 !== "") && (storedValue !== "")) {
-        confirmedValue2 = storedValue;
-        console.log("Confirmed Value 2 is " + confirmedValue2);
+const buttonThree = document.querySelector(".three");
+buttonThree.addEventListener("click", () => {
+    firstValue += 3;
+    console.log("firstValue is " + firstValue);
+    display.textContent = firstValue;
+})
+
+const buttonOperator = document.querySelector(".operator");
+buttonOperator.addEventListener("click", () => {
+    if (firstValue === "") {
+        alert("Enter a value before using operator");
+    } else if (firstValueConfirmed !== "") {
+        alert("Addition is already selected, press equal sign or modify your number");
+        console.log("firstValue is " + firstValue);
+        console.log("firstValueConfirmed is " + firstValueConfirmed);
     }
-    else if (storedValue === "") {
-        location.reload();
+    else { firstValueConfirmed = firstValue;
+    console.log("firstValueConfirmed is " + firstValueConfirmed);
+    display2.textContent = firstValue + "+";
+//  firstValue += "+";
+    display.textContent = "";
+    firstValue = ""; 
+    }
+})
+
+const buttonEqualize = document.querySelector(".equalize");
+buttonEqualize.addEventListener("click", () => {
+    if ((firstValue !== "") && (firstValueConfirmed !== "")) {
+        let firstValueNum = Number(firstValue);
+        let firstValueNConfirmedum = Number(firstValueConfirmed);
+        result = firstValueNum + firstValueNConfirmedum;
+        console.log("result is " + result);
+
     } else {
-        confirmedValue1 = storedValue;
-        console.log("Confirmed Value 1 is " + confirmedValue1);
-        // storedValue becomes = "" again
+        alert("Insufficient info for calculation");
     }
-    storedValue = "";
-    console.log("storedValue is "+ storedValue);
-});
+})
