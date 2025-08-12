d52baa6a761ef7d07b239d625b5926592f1208d1
japuus <50738467+japuus@users.noreply.github.com>
2025-08-12 10:10:01 +0300

    Commited just in case after long break

--- script.js
+++ script.js
@@ -3,46 +3,31 @@ let addedValueConfirmed = "";
 let subtractedValueConfirmed = "";
 let dividedValueConfirmed = "";
 let multipliedValueConfirmed = "";
-const display = document.querySelector(".display");
+let result;
+const display1 = document.querySelector(".display1");
 const display2 = document.querySelector(".display2");
-const display3 = document.querySelector(".display3");
 const buttonOperators = document.querySelectorAll(".operator");
 const buttonAll = document.querySelectorAll(".btn");
-const buttonNums = document.querySelectorAll(".num");
-let result;
+const buttonDot = document.querySelector(".dot");
 
+
+// Getting values from number buttons
+const buttonNums = document.querySelectorAll(".num");
 buttonNums.forEach(button => {
 button.addEventListener("click", () => {
     const value = button.textContent;
     firstValue += value;
     console.log("firstValue is " + firstValue);
-    display.textContent = firstValue;
+    display1.textContent = firstValue;
+    if ((display1.textContent.includes(".")) || (firstValue.includes("."))) {
+        buttonDot.disabled = true;
+    } else {
+        buttonDot.disabled = false;
+    }
   });
 });
 
-/*
-const buttonOne = document.querySelector(".one");
-buttonOne.addEventListener("click", () => {
-    firstValue += 1;
-    console.log("firstValue is " + firstValue);
-    display.textContent = firstValue;
-});
-
-const buttonTwo = document.querySelector(".two");
-buttonTwo.addEventListener("click", () => {
-    firstValue += 2;
-    console.log("firstValue is " + firstValue);
-    display.textContent = firstValue;
-})
-
-const buttonThree = document.querySelector(".three");
-buttonThree.addEventListener("click", () => {
-    firstValue += 3;
-    console.log("firstValue is " + firstValue);
-    display.textContent = firstValue;
-})
-    */
-
+// Addition
 const buttonPlus = document.querySelector(".plus");
 buttonPlus.addEventListener("click", () => {
     if (firstValue === "") {
@@ -52,13 +37,14 @@ buttonPlus.addEventListener("click", () => {
         addedValueConfirmed = firstValue;
     console.log("addedValueConfirmed is " + addedValueConfirmed);
     display2.textContent = addedValueConfirmed + "+";
-    display.textContent = "";
+    display1.textContent = "";
     firstValue = ""; 
     buttonOperators.forEach(button => button.disabled = true);
+    buttonDot.disabled = false;
     }
 })
 
-// Vähennyslasku
+// Subtraction
 const buttonMinus = document.querySelector(".minus");
 buttonMinus.addEventListener("click", () => {
     if (firstValue === "") {
@@ -67,27 +53,15 @@ buttonMinus.addEventListener("click", () => {
         subtractedValueConfirmed = firstValue;
         console.log("subtractedValueConfirmed is " + subtractedValueConfirmed);
         display2.textContent = subtractedValueConfirmed + "-";
-        display.textContent = "";
+        display1.textContent = "";
         firstValue = ""; 
         buttonOperators.forEach(button => button.disabled = true);
+        buttonDot.disabled = false;
         }
 }
 )
 
-const buttonDivide = document.querySelector(".divide");
-buttonDivide.addEventListener("click", () =>  {
-    if (firstValue === "") {
-        alert("Enter a value before using division");
-    } else {
-        dividedValueConfirmed = firstValue;
-        console.log("dividedValueConfirmed is " + dividedValueConfirmed);
-        display2.textContent = dividedValueConfirmed + "/";
-        display.textContent = "";
-        firstValue = "";
-        buttonOperators.forEach(button => button.disabled = true);
-    }
-})
-
+// Multiplication
 const buttonMultiply = document.querySelector(".multiply");
 buttonMultiply.addEventListener("click", () => {
     if (firstValue === "") {
@@ -95,36 +69,52 @@ buttonMultiply.addEventListener("click", () => {
     } else {
         multipliedValueConfirmed = firstValue;
         console.log("multipliedValueConfirmed is " + multipliedValueConfirmed);
-        display2.textContent = multipliedValueConfirmed + "*";
-        display.textContent = "";
+        display2.textContent = multipliedValueConfirmed + "×";
+        display1.textContent = "";
         firstValue = "";
         buttonOperators.forEach(button => button.disabled = true);
+        buttonDot.disabled = false;
     }
 })
 
+// Division
+const buttonDivide = document.querySelector(".divide");
+buttonDivide.addEventListener("click", () =>  {
+    if (firstValue === "") {
+        alert("Enter a value before using division");
+    } else {
+        dividedValueConfirmed = firstValue;
+        console.log("dividedValueConfirmed is " + dividedValueConfirmed);
+        display2.textContent = dividedValueConfirmed + "÷";
+        display1.textContent = "";
+        firstValue = "";
+        buttonOperators.forEach(button => button.disabled = true);
+        buttonDot.disabled = false;
+    }
+})
+
+// Getting result
 const buttonEqualize = document.querySelector(".equalize");
 buttonEqualize.addEventListener("click", () => {
     if ((firstValue !== "") && (addedValueConfirmed !== "")) {
         let result = Number(firstValue) + Number(addedValueConfirmed);
-        display.textContent = "";
+        display1.textContent = "";
         display2.textContent += (firstValue + "=" + result)
         buttonAll.forEach(button => button.disabled = true);
     } else if ((firstValue !== "") && (subtractedValueConfirmed !== "")) {
         let result = Number(subtractedValueConfirmed) - Number(firstValue);
-        display.textContent = "";
+        display1.textContent = "";
         display2.textContent += (firstValue + "=" + result);
         buttonAll.forEach(button => button.disabled = true);
     }  else if ((firstValue !== "") && (dividedValueConfirmed !== "")) {
         let result = Number(dividedValueConfirmed) / Number(firstValue);
-//      return Number.isInteger(result) ? result : parseFloat(result.toFixed(3));
         if (Number.isInteger(result)) {
             display2.textContent += (firstValue + "=" + result);
         } else {
             let resultRounded = result.toFixed(4);
             display2.textContent += (firstValue + "=" + resultRounded);
         }
-        display.textContent = "";
-//      display2.textContent += (firstValue + "=" + resultRounded);
+        display1.textContent = "";
         buttonAll.forEach(button => button.disabled = true);
     }  else if ((firstValue !== "") && (multipliedValueConfirmed !== "")) {
         let result = Number(multipliedValueConfirmed) * Number(firstValue);
@@ -134,7 +124,7 @@ buttonEqualize.addEventListener("click", () => {
             let resultRounded = result.toFixed(4);
             display2.textContent += (firstValue + "=" + resultRounded);
         }
-        display.textContent = "";
+        display1.textContent = "";
         buttonAll.forEach(button => button.disabled = true);
     }
     else {
@@ -142,7 +132,17 @@ buttonEqualize.addEventListener("click", () => {
     }
 })
 
+// Clear
 const buttonReset = document.querySelector(".reset");
 buttonReset.addEventListener("click", () => {
     location.reload();
 })
+
+// Backspace
+const backspaceButton = document.querySelector(".backspace");
+backspaceButton.addEventListener("click", () => {
+  firstValue = firstValue.slice(0, -1);
+  display1.textContent = firstValue;
+  console.log("firstValue is " + firstValue);
+  buttonDot.disabled = firstValue.includes(".");
+});
